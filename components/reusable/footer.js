2ea6aff4ee58af550c3f02ec066a0e176cc8cbac
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2017-12-17 15:23:46 -0800

    footer link changed to actual domain and new github account

--- components/reusable/footer.js
+++ components/reusable/footer.js
@@ -4,14 +4,14 @@ import { View, Text, StyleSheet, Image, Linking, TouchableOpacity } from 'react-
 const Footer = () => {
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={()=>Linking.openURL('https://sanheng03.github.io')}>
+      <TouchableOpacity onPress={()=>Linking.openURL('https://mj-webdev.com')}>
         <Image style={styles.abouticon} source={require('./img/about.png')} />
       </TouchableOpacity>
       <View>
         <Text style={styles.text}>© Copyright 2017</Text>
         <Text style={styles.text}>Minhyeong Billy Joe</Text>
       </View>
-      <TouchableOpacity onPress={()=>Linking.openURL('https://github.com/sanheng03')}>
+      <TouchableOpacity onPress={()=>Linking.openURL('https://github.com/minhyeong-joe')}>
         <Image style={styles.githubicon} source={require('./img/github.png')} />
       </TouchableOpacity>
       <TouchableOpacity onPress={()=>Linking.openURL('https://expo.io/@sanheng03')}>
